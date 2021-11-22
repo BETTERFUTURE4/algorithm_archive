336db39e8272c5b471c8e1345bfbe82d74b90260
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-22 20:07:54 +0900

    Add) Implement project6

--- DrawPyramid/src/ShapePrinter.java
+++ DrawPyramid/src/ShapePrinter.java
@@ -1,6 +1,16 @@
 public class ShapePrinter {
     public void printPyramid(int height) {
         // 코드를 입력하세요.
-        System.out.println("print pyramid");
+        for (int i = 1; i <= height; i++) {
+            String row = "";
+            for (int n = 0; n < height-i; n++) {
+                row += " ";
+            }
+            for (int n = 0; n < 2*i - 1; n++) {
+                row += "*";
+            }
+            System.out.println(row);
+
+        }
     }
 }
