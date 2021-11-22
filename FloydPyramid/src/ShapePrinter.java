d7653b662b600d654300cc78ed922f5882f716ce
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-22 20:28:56 +0900

    Add) method ver1 : not space

--- FloydPyramid/src/ShapePrinter.java
+++ FloydPyramid/src/ShapePrinter.java
@@ -1,6 +1,14 @@
 public class ShapePrinter {
     public void printFloydsPyramid(int height) {
         // 코드를 입력하세요.
+        int num = 1;
+        for (int i = 1; i <= height; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
         System.out.println("print Tri");
     }
 }
