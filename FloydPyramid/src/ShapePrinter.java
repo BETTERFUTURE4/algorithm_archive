d59c7ee1b7406e814a57fdd0d198353eb5ea8da4
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-22 20:48:31 +0900

    Implement) rename, method finish

--- FloydPyramid/src/ShapePrinter.java
+++ FloydPyramid/src/ShapePrinter.java
@@ -2,13 +2,22 @@ public class ShapePrinter {
     public void printFloydsPyramid(int height) {
         // 코드를 입력하세요.
         int num = 1;
-        for (int i = 1; i <= height; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print(num + " ");
+        int maxLength = Integer.toString(height * (height + 1) / 2).length();
+        int numLength;
+        
+        for (int row = 1; row <= height; row++) {
+            String line = "";
+            for (int col = 0; col < row; col++) {
+                numLength = Integer.toString(num).length();
+
+                for (int k = 0; k < (maxLength - numLength); k++) {
+                    line += " ";
+                }
+
+                line += num + " ";
                 num++;
             }
-            System.out.println();
+            System.out.println(line);
         }
-        System.out.println("print Tri");
     }
 }
