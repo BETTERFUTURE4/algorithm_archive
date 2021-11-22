321f6064683c4ca73983945b2ce5c09018295d75
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-23 08:55:35 +0900

    Feat : change convention

--- UnitConverter/src/UnitConverter.java
+++ UnitConverter/src/UnitConverter.java
@@ -1,9 +1,9 @@
 public class UnitConverter {
     // 필요한 상수 정의
     public static final double KILOGRAMS_PER_POUND = 0.45359237;
-    public static final double POUNDS_PER_KILOGRAM = 1.0 / 0.45359237;
+    public static final double POUNDS_PER_KILOGRAM = 1 / KILOGRAMS_PER_POUND;
     public static final double CENTIMETERS_PER_INCH = 2.54;
-    public static final double INCHES_PER_CENTIMETER = 1.0 / 2.54;
+    public static final double INCHES_PER_CENTIMETER = 1 / CENTIMETERS_PER_INCH;
 
     private UnitConverter() {}
 
@@ -25,10 +25,10 @@ public class UnitConverter {
     }
     public static double toFahrenheit(double celsius) {
         // 메소드 내부를 채워주세요
-        return celsius * (9.0 / 5.0) + 32.0;
+        return celsius * 9 / 5 + 32;
     }
     public static double toCelsius(double fahrenheit) {
         // 메소드 내부를 채워주세요
-        return (fahrenheit - 32.0) * (5.0 / 9.0);
+        return (fahrenheit - 32) * 5 / 9;
     }
 }
