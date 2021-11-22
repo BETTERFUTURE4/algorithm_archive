83893cc981258e7809a3bde170a872fda32cf70d
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-22 19:10:59 +0900

    New) project 6 : main, ShapePrinter

---
+++ DrawPyramid/src/ShapePrinter.java
@@ -0,0 +1,6 @@
+public class ShapePrinter {
+    public void printPyramid(int height) {
+        // 코드를 입력하세요.
+        System.out.println("print pyramid");
+    }
+}
--- DrawTriangle/src/ShapePrinter.java
+++ DrawTriangle/src/ShapePrinter.java
@@ -1,6 +1,11 @@
 public class ShapePrinter {
     public void printTriangle(int height) {
         // 코드를 입력하세요.
-        System.out.println("print 직각 이등변 삼각형");
+        for (int i = 1; i <= height; i++) {
+            for (int ii = 1; ii <= i; ii++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
