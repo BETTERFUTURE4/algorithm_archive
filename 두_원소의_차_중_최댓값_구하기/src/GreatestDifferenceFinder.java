73baad3a60f7022ea196e9b2409fca7eaf203618
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-22 17:34:46 +0900

    Add) case: intArray.length < 2

--- 두_원소의_차_중_최댓값_구하기/src/GreatestDifferenceFinder.java
+++ 두_원소의_차_중_최댓값_구하기/src/GreatestDifferenceFinder.java
@@ -2,6 +2,10 @@ public class GreatestDifferenceFinder {
     int greatestDifference(int[] intArray) {
         // 코드를 입력하세요.
         int answer = 0;
+        // 원소가 0개 또는 1개면 0 리턴
+        if (intArray.length < 2) {
+            return 0;
+        }
         for (int i = 0; i < intArray.length - 1; i++) {
             for (int ii = i + 1; ii < intArray.length; ii++) {
                 int difference = Math.abs(intArray[i] - intArray[ii]);
