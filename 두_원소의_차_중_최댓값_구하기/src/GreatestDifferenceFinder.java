fa8b3cb4261971e8304c593aebb5337550833f0f
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-22 17:30:55 +0900

    Implement) greatestDifference method

--- 두_원소의_차_중_최댓값_구하기/src/GreatestDifferenceFinder.java
+++ 두_원소의_차_중_최댓값_구하기/src/GreatestDifferenceFinder.java
@@ -2,6 +2,14 @@ public class GreatestDifferenceFinder {
     int greatestDifference(int[] intArray) {
         // 코드를 입력하세요.
         int answer = 0;
+        for (int i = 0; i < intArray.length - 1; i++) {
+            for (int ii = i + 1; ii < intArray.length; ii++) {
+                int difference = Math.abs(intArray[i] - intArray[ii]);
+                if(difference > answer) {
+                    answer = difference;
+                }
+            }
+        }
         return answer;
     }
 }
