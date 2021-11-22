9d86dd5929263534e8de63bbb6a539b2c1ccd2d3
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-22 17:37:03 +0900

    Change) set max, min and sub them

--- 두_원소의_차_중_최댓값_구하기/src/GreatestDifferenceFinder.java
+++ 두_원소의_차_중_최댓값_구하기/src/GreatestDifferenceFinder.java
@@ -1,19 +1,21 @@
 public class GreatestDifferenceFinder {
     int greatestDifference(int[] intArray) {
-        // 코드를 입력하세요.
-        int answer = 0;
         // 원소가 0개 또는 1개면 0 리턴
         if (intArray.length < 2) {
             return 0;
         }
-        for (int i = 0; i < intArray.length - 1; i++) {
-            for (int ii = i + 1; ii < intArray.length; ii++) {
-                int difference = Math.abs(intArray[i] - intArray[ii]);
-                if(difference > answer) {
-                    answer = difference;
-                }
+        int max = intArray[0];
+        int min = intArray[0];
+
+        for (int i = 0; i < intArray.length; i++) {
+            if (max < intArray[i]) {
+                max = intArray[i];
+            }
+
+            if (min > intArray[i]) {
+                min = intArray[i];
             }
         }
-        return answer;
+        return max - min;
     }
 }
