28285dec4cfe26ed1f189ee5dd8f87da9af8d2ea
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-23 08:47:11 +0900

    Feat : Change 4 methods, int to double

--- UnitConverter/src/UnitConverter.java
+++ UnitConverter/src/UnitConverter.java
@@ -1,30 +1,34 @@
 public class UnitConverter {
     // 필요한 상수 정의
     public static final double KILOGRAMS_PER_POUND = 0.45359237;
-    public static final double POUNDS_PER_KILOGRAM = (double) 1 / 0.45359237;
+    public static final double POUNDS_PER_KILOGRAM = 1.0 / 0.45359237;
     public static final double CENTIMETERS_PER_INCH = 2.54;
-    public static final double INCHES_PER_CENTIMETER = (double) 1 / 2.54;
+    public static final double INCHES_PER_CENTIMETER = 1.0 / 2.54;
+
+    private UnitConverter() {}
 
     public static double toPounds(double kilograms) {
         // 메소드 내부를 채워주세요
-        return kilograms * KILOGRAMS_PER_POUND;
+        return kilograms * POUNDS_PER_KILOGRAM;
     }
     public static double toKilograms(double pounds) {
         // 메소드 내부를 채워주세요
-        return pounds * POUNDS_PER_KILOGRAM;
+        return pounds * KILOGRAMS_PER_POUND;
     }
     public static double toCentimeters(double inches) {
         // 메소드 내부를 채워주세요
-        return  inches * INCHES_PER_CENTIMETER;
+        return  inches * CENTIMETERS_PER_INCH;
     }
     public static double toInches(double centimeters) {
         // 메소드 내부를 채워주세요
-        return centimeters * CENTIMETERS_PER_INCH;
+        return centimeters * INCHES_PER_CENTIMETER;
     }
     public static double toFahrenheit(double celsius) {
         // 메소드 내부를 채워주세요
+        return celsius * (9.0 / 5.0) + 32.0;
     }
     public static double toCelsius(double fahrenheit) {
         // 메소드 내부를 채워주세요
+        return (fahrenheit - 32.0) * (5.0 / 9.0);
     }
 }
