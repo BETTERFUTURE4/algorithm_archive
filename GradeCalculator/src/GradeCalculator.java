63e8ca1edd496025b09d14f4ab55cf7b2b393991
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-23 20:21:44 +0900

    Feat : Implement all methods

--- GradeCalculator/src/GradeCalculator.java
+++ GradeCalculator/src/GradeCalculator.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class GradeCalculator {
+import java.util.ArrayList;
+
+public class GradeCalculator {
+    public String name;
+    public double grade, nowGrade, sumGrade, fullCredit, nowFullCredit;
+    public ArrayList<Course> courseArray = new ArrayList<>();
+    public int credit, nowCredit;
+
+    public GradeCalculator(String name, double grade, int credit) {
+        this.name = name;
+        this.grade = grade;
+        this.credit = credit;
+        this.fullCredit = grade * credit;
+        this.sumGrade = grade;
+    }
+
+    public void addCourse(Course course) {
+        courseArray.add(course);
+        nowCredit += course.credit;
+        nowFullCredit += course.grade * course.credit;
+        nowGrade = nowFullCredit / nowCredit;
+        sumGrade = (fullCredit + nowFullCredit) / (credit + nowCredit);
+    }
+
+    public void print() {
+        System.out.println("직전 학기 성적: "+ grade +" (총 " + credit + "학점)");
+        System.out.println("이번 학기 성적: "+ String.format("%.10f", nowGrade) +" (총 " + nowCredit + "학점)");
+        System.out.println("전체 예상 학점: "+ String.format("%.10f", sumGrade) +" (총 " + (credit+nowCredit) + "학점)");
+    }
+
 }
