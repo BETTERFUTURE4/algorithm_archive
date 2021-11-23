a68dcbae4b393136f1ddf32f4b2d3077024d4934
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-23 09:38:03 +0900

    Feat : Make overall format

---
+++ GeneralSurvival/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static int getSurvivingIndex(int n, int k) {
+        // 코드를 입력하세요.
+    }
+
+    public static void main(String[] args) {
+        System.out.println("김신은 " + getSurvivingIndex(20, 5) + "번 자리에 서있으면 됩니다.");
+    }
+}
