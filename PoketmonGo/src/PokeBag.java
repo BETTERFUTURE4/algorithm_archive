817cf7543e3a60e09e1cff4b0067ed7108cd85ff
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-23 11:48:54 +0900

    Feat : Make overall format

---
+++ PoketmonGo/src/PokeBag.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class PokeBag {
+    private final 자료형 pokemons = new 자료형();
+
+    public ArrayList<Pokemon> getPokemons(String name) {
+    }
+
+    public void add(Pokemon pokemon) {
+    }
+
+    public Pokemon getStrongest(String name) {
+    }
+
+    public Pokemon getStrongest() {
+    }
+}
