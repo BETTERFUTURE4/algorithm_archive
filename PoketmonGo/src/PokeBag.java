0e72306a0130420de6a719bb8f20b44d26cfee80
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-23 13:33:02 +0900

    Feat : finish code

--- PoketmonGo/src/PokeBag.java
+++ PoketmonGo/src/PokeBag.java
@@ -1,17 +1,52 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PokeBag {
-    private final 자료형 pokemons = new 자료형();
+    private HashMap<String,ArrayList<Pokemon>> pokemons = new HashMap<>();
 
     public ArrayList<Pokemon> getPokemons(String name) {
+        return pokemons.get(name);
     }
 
     public void add(Pokemon pokemon) {
+        String name = pokemon.name;
+
+        // 해당하는 ArrayList가 없으면 생성
+        if (getPokemons(name) == null) {
+            pokemons.put(name, new ArrayList<Pokemon>());
+        }
+
+        getPokemons(name).add(pokemon);
     }
 
     public Pokemon getStrongest(String name) {
+        if (getPokemons(name) == null) {
+            return null;
+        }
+
+        Pokemon strongestPokemon = null;
+        int maxCp = 0;
+
+        for (Pokemon p : getPokemons(name)) {
+            if (strongestPokemon == null || p.cp > maxCp) {
+                strongestPokemon = p;
+                maxCp = p.cp;
+            }
+        }
+        return strongestPokemon;
+
     }
 
     public Pokemon getStrongest() {
+        Pokemon strongestPokemon = new Pokemon("",0);
+        int maxCp = 0;
+        for (String key : pokemons.keySet()) {
+            Pokemon maxP = getStrongest(key);
+            if (maxP.cp > maxCp) {
+                strongestPokemon = getStrongest(key);
+                maxCp = getStrongest(key).cp;
+            }
+        }
+        return strongestPokemon;
     }
 }
