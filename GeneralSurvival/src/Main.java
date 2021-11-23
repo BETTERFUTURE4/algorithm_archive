9711a45b37a4d1b933b9b99f1d08bfc1a9c6ce3f
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-23 10:58:29 +0900

    Feat : Change method logic

--- GeneralSurvival/src/Main.java
+++ GeneralSurvival/src/Main.java
@@ -3,6 +3,21 @@ import java.util.ArrayList;
 public class Main {
     public static int getSurvivingIndex(int n, int k) {
         // 코드를 입력하세요.
+        ArrayList<Integer> member = new ArrayList<>();
+        // 멤버 추가
+        for (int memberIndex = 1; memberIndex <= n; memberIndex++) {
+            member.add(memberIndex);
+        }
+        // 변수 인덱스 선언
+        int killMemberIndex = 0;
+        
+        // 한명씩 죽이기
+        while (member.size() > 1) {
+            killMemberIndex = (killMemberIndex + k-1) % member.size();
+            System.out.println(member.remove(killMemberIndex) + "번 군사가 죽습니다.");
+        }
+        // 마지막 자리 리턴
+        return member.get(0);
     }
 
     public static void main(String[] args) {
