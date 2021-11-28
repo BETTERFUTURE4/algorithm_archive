36d25c568b2a1109b34352abe52303d243fb37ca
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-28 12:00:31 +0900

    feat(CardShuffler): 프로젝트 구현

--- CardShuffler/src/Card.java
+++ CardShuffler/src/Card.java
@@ -1,2 +1,34 @@
 public class Card {
+	public final int suitNumber;
+	public final int rankNumber;
+	public static final String[] suitStrings = {"Clubs", "Hearts", "Diamonds", "Spades"};
+
+	public Card(int suitNumber, int rankNumber) {
+		this.suitNumber = suitNumber;
+		this.rankNumber = rankNumber;
+	}
+
+	public String getSuit() {
+		return suitStrings[suitNumber - 1];
+	}
+
+	public String getRank() {
+		if (rankNumber == 1) {
+			return "Ace";
+		}
+		if (rankNumber == 11) {
+			return "Jack";
+		}
+		if (rankNumber == 12) {
+			return "Queen";
+		}
+		if (rankNumber == 13) {
+			return "King";
+		}
+		return rankNumber + "";
+	}
+
+	public String toString() {
+		return getRank() + " of " + getSuit();
+	}
 }
--- CardShuffler/src/Deck.java
+++ CardShuffler/src/Deck.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Deck {
+	private ArrayList<Card> cards = new ArrayList<>();
+
+	public ArrayList<Card> getCards() {
+		return cards;
+	}
+
+	public void addCard(Card card) {
+		cards.add(card);
+	}
+
+	public void print() {
+		for (Card card : getCards()) {
+			System.out.println(card);
+		}
+	}
+
+	public void shuffle() {
+		Random random = new Random();
+		ArrayList<Card> newCards = new ArrayList<>();
+		while (!cards.isEmpty()) {
+			newCards.add(cards.remove(random.nextInt(cards.size())));
+		}
+		cards = newCards;
+	}
+
+	public Deck deal(int count) {
+		Deck hand = new Deck();
+		for (int i = 0; i < count; i++) {
+			hand.addCard(cards.remove(cards.size() - 1));
+		}
+		return hand;
+	}
 }
